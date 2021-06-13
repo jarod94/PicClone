8c4c3ff87c25787581d3519678fe9bb683aba5c3
Amorek Azonsou <hi@amorekazonsou.com>
2021-06-13 10:59:20 +0100

    Picture Clone

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,54 @@
+class UsersController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
+  before_action :set_user, only: %i[ show edit update ]
+  before_action :guard_action_not_current_user, only: [:edit, :update]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      render :new
+    end
+  end
+
+  def show
+    # @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      redirect_to user_path, notice: "userを編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  # private
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation,
+                                 :image, :image_cache )
+  end
+
+  # def user_params_update
+  #   params.require(:user).permit(:name, :email)
+  # end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def guard_action_not_current_user
+    if current_user.id != @user.id
+      redirect_to user_path(@user.id), notice:"他の方のprofileは変更できません"
+    end
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # get 'favorites/index'  #コメントアウト 0318 k.nomura
+  # get 'sessions/new'  #コメントアウト 0318 k.nomura
+  resources :pictures do
+    collection do
+      post :confirm
+    end
+  end
+  resources :users, only: [:new, :create, :show, :edit, :update]
+  resources :sessions, only: [:new, :create, :destroy]
+  root to: 'pictures#index'
+  resources :favorites, only: [:create, :destroy, :index]
+end
